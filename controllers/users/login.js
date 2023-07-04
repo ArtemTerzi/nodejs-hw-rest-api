cdd764b186b36928081d2e11663da0ce2a93c778
Artem Terzi <terzi.artem.yuriyovych@gmail.com>
2023-07-04 20:19:34 +0300

    ADDED: Ctrls + Dockerfile + helpers + routers
    Ctrls:
    + verifyEmail
    + resendVerifyEmail
    *Update ctrls: register and login
    
    helpers:
    + sendEmail
    
    routes:
    +GET '/verify/:verificationToken'
    + POST '/verify'
    
    + Dockerfile

--- controllers/users/login.js
+++ controllers/users/login.js
@@ -11,6 +11,8 @@ const login = async (req, res) => {
   const user = await User.findOne({ email });
   if (!user) throw HttpError(401, 'Email or password is wrong');
 
+  if (!user.verify) throw HttpError(401, 'Email not verified');
+
   const passwordCompare = await bcrypt.compare(password, user.password);
   if (!passwordCompare) throw HttpError(401, 'Email or password is wrong');
 
